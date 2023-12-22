501b86d46d50753dea0088d3dd1991bee4e0c12b
Edoardo Luigi Altieri <100417984+Wardy48@users.noreply.github.com>
2023-12-22 23:38:09 +0100

    Complete (at the base level)
    All the minimum requirements for the project have been implemented.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,21 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
-public class SceneInfo
-{
-    // Make sceneNumber a public static variable
-    public static int sceneNumber;
-
-    public static void UpdateSceneNumber()
-    {
-        // Get the current active scene
-        Scene sceneLoaded = SceneManager.GetActiveScene();
-
-        // Update the sceneNumber
-        sceneNumber = sceneLoaded.buildIndex;
-    }
-}
+using System.IO;
+using Microsoft.SqlServer.Server;
 
 public class MainManager : MonoBehaviour
 {
@@ -26,57 +13,96 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
-    
+    public int HighScore;
+    public string HighScorer;
     public GameObject GameOverText;
+    public Text HighScoreText;
     
     private bool m_Started = false;
     private int m_Points;
     
     private bool m_GameOver = false;
+    private int sceneIndex;
+
+    void Awake()
+    {
+        LoadHighScoreData();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
+        // Get the current active scene
+        Scene sceneLoaded = SceneManager.GetActiveScene();
 
-        string playerName = PlayerPrefs.GetString("NameChosenByUser");        
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
+        // Update the sceneIndex
+        sceneIndex = sceneLoaded.buildIndex;
+        if(sceneIndex == 1)
         {
-            for (int x = 0; x < perLine; ++x)
+            const float step = 0.6f;
+            int perLine = Mathf.FloorToInt(4.0f / step);
+
+            string playerName = PlayerPrefs.GetString("NameChosenByUser");
+            
+            int[] pointCountArray = new [] {1,1,2,2,5,5};
+            for (int i = 0; i < LineCount; ++i)
             {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
+                for (int x = 0; x < perLine; ++x)
+                {
+                    Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                    var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                    brick.PointValue = pointCountArray[i];
+                    brick.onDestroyed.AddListener(AddPoint);
+                }
+            }        
+            
+            ScoreText.text = $"{playerName}'s score: {m_Points}";
+            
+            if (HighScore != 0 && HighScore != 1)
+            {
+                HighScoreText.text = "High score: " + HighScore + " points, by " + HighScorer;
             }
-        }        
-        
-        ScoreText.text = $"{playerName}'s score: {m_Points}";
+            else if (HighScore == 1)
+            {
+                HighScoreText.text = "High score: " + HighScore + " point, by " + HighScorer;
+            }
+            else
+            {
+                HighScoreText.text = "No high score to show. Make this count, " + playerName + "!";
+            }
+        }
     }
 
     private void Update()
     {
-        if (!m_Started)
+        if(sceneIndex == 1)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (!m_Started)
             {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    m_Started = true;
+                    float randomDirection = Random.Range(-1.0f, 1.0f);
+                    Vector3 forceDir = new Vector3(randomDirection, 1, 0);
+                    forceDir.Normalize();
 
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+                    Ball.transform.SetParent(null);
+                    Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+                }
             }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
+            else if (m_GameOver)
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+            }
+            
+            if (m_Points > HighScore)
+            {
+                HighScore = m_Points;
+                string playerName = PlayerPrefs.GetString("NameChosenByUser");
+                HighScorer = playerName;
             }
         }
     }
@@ -90,7 +116,60 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        SaveHighScoreData();
         m_GameOver = true;
         GameOverText.SetActive(true);
+        if (HighScore != 1)
+        {
+            HighScoreText.text = "High score: " + HighScore + " points, by " + HighScorer;
+        }
+        else if (HighScore == 1)
+        {
+            HighScoreText.text = "High score: " + HighScore + " point, by " + HighScorer;
+        }
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int HighScore;
+        public string HighScorer;
+    }
+
+    public void SaveHighScoreData()
+    {
+        SaveData data = new SaveData();
+        data.HighScore = HighScore;
+        data.HighScorer = HighScorer;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadHighScoreData()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            HighScorer = data.HighScorer;
+            HighScore = data.HighScore;
+        }        
+    }
+
+    public void DeleteHighScoreData()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/StartMenuUIManager.cs
+++ Assets/Scripts/StartMenuUIManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 using JetBrains.Annotations;
+using Unity.PlasticSCM.Editor.WebApi;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -14,13 +16,13 @@ using UnityEditor;
 public class StartMenuUIManager : MonoBehaviour
 {
     [SerializeField] TMP_InputField nameInputField; // Reference to the InputField component
+    [SerializeField] TMP_Text highScoreInfo;
 
     // This method is called when the player finishes editing the text field
     public void OnValueChanged()
     {
         // Save the player's name in PlayerPrefs
         PlayerPrefs.SetString("NameChosenByUser", nameInputField.text);
-
         // Save changes to disk
         PlayerPrefs.Save();
     }
@@ -28,11 +30,34 @@ public class StartMenuUIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        MainManager mainManager = GameObject.Find("MainManager").GetComponent<MainManager>();
+        int highScore = mainManager.HighScore;
+        string highScorer = mainManager.HighScorer;
+        if (highScore != 0 && highScore != 1)
+        {
+            highScoreInfo.text = "Current high scorer: " + highScorer + ", with " + highScore + " points.";
+        }
+        else if (highScore == 1)
+        {
+            highScoreInfo.text = "Current high scorer: " + highScorer + ", with " + highScore + " point.";
+        }
+        else
+        {
+            highScoreInfo.text = "No high score to show just yet!";
+        }
+    }
 
+    public void ShowHighScoreWasReset()
+    {
+        highScoreInfo.text = "No high score to show just yet!";
     }
 
     public void StartNew()
     {
+        if (string.IsNullOrEmpty(nameInputField.text))
+        {
+            PlayerPrefs.SetString("NameChosenByUser", "Anon");
+        }
         SceneManager.LoadScene(1);
     }
 
